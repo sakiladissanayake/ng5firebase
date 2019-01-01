4f23507bd53e81842da4741767fb5cdcdec06274
sakiladissanayake <sakilad@gmail.com>
2019-01-01 09:36:42 +0530

    add firebase configurations

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCjKg-sfPCWXDK9C2y_hYQCjLZKFxNDT-g',
+    authDomain: 'shareapp-b4b5c.firebaseapp.com',
+    databaseURL: 'https://shareapp-b4b5c.firebaseio.com',
+    projectId: 'shareapp-b4b5c',
+    storageBucket: 'shareapp-b4b5c.appspot.com',
+    messagingSenderId: '944202080954'
+  }
 };
